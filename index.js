adfce64b661707f65fdcfd297df990ab0b5a1579
lapron3000 <alejandroparparcen@yahoo.com>
2020-09-08 19:25:32 -0500

    Created a Simple bootstrap form using most elements

---
+++ index.js
@@ -0,0 +1,15 @@
+const express= require("express");
+const app = express();
+const path = require("path");
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'example.html'));
+});
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/public/js', express.static(path.join(__dirname, 'node_modules/bs-custom-file-input/dist')))
+
+app.listen(3000, () => {
+  console.log("server running 3000");
+});
