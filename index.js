ae1d0be2c2ca62b23e7d7a668da879c0b8c58c78
lapron3000 <alejandroparparcen@yahoo.com>
2020-09-08 19:48:27 -0500

    added a new landing page as example

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express= require("express");
+const express = require("express");
 const app = express();
 const path = require("path");
 
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'example.html'));
+  res.sendFile(path.join(__dirname, "example.html"));
+});
+app.post("/enviado", (req, res) => {
+  res.sendFile(path.join(__dirname, "enviado.html"));
 });
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use('/public/js', express.static(path.join(__dirname, 'node_modules/bs-custom-file-input/dist')))
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(
+  "/public/js",
+  express.static(path.join(__dirname, "node_modules/bs-custom-file-input/dist"))
+);
 
 app.listen(3000, () => {
   console.log("server running 3000");
